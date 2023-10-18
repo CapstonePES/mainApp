2fdfd3471d55adcc0065558b3d78bbe7b9794a47
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-10-18 15:50:32 +0530

    basic test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+from model import model
 
 def main():
-    st.title("My Streamlit App")
-    st.write("Welcome to my first Streamlit app!")
-    
+    st.title("This will be the dashboard and stuff")
+    if st.button("Run Model"):
+        with st.spinner("Running Model..."):
+            result = model()
+        st.success("Model finished running!")
+        st.write("Result from model: ",result)
+
 if __name__ == "__main__":
     main()
