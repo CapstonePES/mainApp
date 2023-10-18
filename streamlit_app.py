f10f661141a38e1cd3052c21eb9ccacdeb8a667d
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-10-18 15:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from model import model
+import requests
 
 def main():
     st.title("This will be the dashboard and stuff")
@@ -8,6 +9,14 @@ def main():
             result = model()
         st.success("Model finished running!")
         st.write("Result from model: ",result)
+    if st.button("Get data from thingspeak"):
+        with st.spinner("Getting data..."):
+            co_read = "ZKSZLC0A13S5Y2BV"
+            co_channel = 2303832
+            co_field = 1
+            co_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(co_channel, co_field, co_read)
+            co_data = requests.get(co_url).json()
+            st.write(co_data)
 
 if __name__ == "__main__":
     main()
