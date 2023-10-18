d954083b61c2e4dd84ed6117ecaff44902d00ff0
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-10-18 16:05:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,15 @@ import requests
 import pandas as pd
 
 def main():
+    co_df = pd.DataFrame()
+    pm_df = pd.DataFrame()
     st.title("This will be the dashboard and stuff")
     if st.button("Run Model"):
         with st.spinner("Running Model..."):
             result = model()
         st.success("Model finished running!")
         st.write("Result from model: ",result)
-    if st.button("Get data from thingspeak changed"):
+    if st.button("Get data from thingspeak"):
         with st.spinner("Getting data..."):
             co_read = "ZKSZLC0A13S5Y2BV"
             co_channel = 2303832
@@ -19,8 +21,19 @@ def main():
             co_data = requests.get(co_url).json()
             co_data = co_data['feeds']
             co_df = pd.DataFrame(co_data)
+            pm_read = "PS8QAYOFT2YUUF11"
+            pm_channel = 2253626
+            pm_field = 3
+            pm_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(pm_channel, pm_field, pm_read)
+            pm_data = requests.get(pm_url).json()
+            pm_data = pm_data['feeds']
+            pm_df = pd.DataFrame(pm_data)
+    if co_df.empty == False and pm_df.empty == False:
+        co_col, pm_col = st.columns(2)
+        with co_col:
             st.dataframe(co_df)
-
+        with pm_col:
+            st.dataframe(pm_df)
 
 if __name__ == "__main__":
     main()
