6b654b20bb79923d6684b6a587c33ceba231f2ed
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-11-06 22:37:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,71 +4,113 @@ from test import test_func
 import requests
 import pandas as pd
 
+
 def main():
-    if st.button("pray to god"):
-        st.write("okie call start")
-        resu_thingy = test_func()
-        st.write("Func done????")
-        st.write(resu_thingy)
-    if 'values' not in st.session_state:
-        st.session_state['values']=False
+    # if st.button("pray to god"):
+    #     st.write("okie call start")
+    #     resu_thingy = test_func()
+    #     st.write("Func done????")
+    #     st.write(resu_thingy)
+    if "values" not in st.session_state:
+        st.session_state["values"] = False
     co_df = pd.DataFrame()
     pm_df = pd.DataFrame()
     co_read_key = "ZKSZLC0A13S5Y2BV"
     co_channel = 2303832
     co_field = 1
-    co_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(co_channel, co_field, co_read_key)
+    co_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(
+        co_channel, co_field, co_read_key
+    )
     pm_read_key = "PS8QAYOFT2YUUF11"
     pm_channel = 2253626
     pm_field = 3
-    pm_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(pm_channel, pm_field, pm_read_key)
+    pm_url = "https://api.thingspeak.com/channels/{}/fields/{}.json?api_key={}".format(
+        pm_channel, pm_field, pm_read_key
+    )
     st.title("This will be the dashboard and stuff")
 
-    # accept age 
-    age = st.number_input("Please enter your age: ",step=1,min_value=0,max_value=100)
+    # accept age
+    age = st.number_input("Please enter your age: ", step=1, min_value=0, max_value=100)
     # Choose your gender
-    gender = st.radio(label="gender",options=["Male","Female"])
-    dust = st.slider("On a Scale of 1 to 8, how allergic are you to dust particles?", 1, 8)
-    hazard = st.slider("On a scale of 1 to 8, how would you classify your occupational hazards?", 1, 8)
-    gene = st.slider("On a scale of 1 to 8, how would you classify your genetic risk of lung cancer?", 1, 8)
+    gender = st.radio(label="What's your gender?", options=["Male", "Female"])
+    dust = st.slider(
+        "On a Scale of 1 to 8, how allergic are you to dust particles?", 1, 8
+    )
+    hazard = st.slider(
+        "On a scale of 1 to 8, how would you classify your occupational hazards?", 1, 8
+    )
+    gene = st.slider(
+        "On a scale of 1 to 8, how would you classify your genetic risk of lung cancer?",
+        1,
+        8,
+    )
     lung_disesa = st.slider("Do you currently have any chronic lung disease?", 1, 7)
-    smokin = st.slider("Do you smoke?", 1, 8)
-    pass_smok = st.slider("On a scale of 1 to 8, what would be your exposure to cigarette smoke?", 1, 8)
-    nails = st.slider("Have you noticed any clubbing of finger nails?", 1, 9)
-    cold = st.slider("On a scale of 1 to 7, how frequently do you contract a cold?", 1, 7)
+    smokin = st.slider("On a scale of 1-8 how often do you smoke?", 1, 8)
+    pass_smok = st.slider(
+        "On a scale of 1 to 8, what would be your exposure to cigarette smoke?", 1, 8
+    )
+    nails = st.slider(
+        "Have you noticed any clubbing of finger nails? If yes how extreme is it?", 1, 9
+    )
+    cold = st.slider(
+        "On a scale of 1 to 7, how frequently do you contract a cold?", 1, 7
+    )
     a = st.button("Submit")
-    if a or st.session_state['values']:
-        st.session_state['values']=True
-        st.text("Age: "+str(age)+"\nGender: "+str(gender)+ "\ndust"+str(dust)+"\n hazard"+str(hazard)+"\n gene"+str(gene)+"\n lung_disesa"+str(lung_disesa)+"\n smokin"+str(smokin)+"\n pass_smok"+str(pass_smok)+"\n nails"+str(nails)+"\n cold"+str(cold))
+    if a or st.session_state["values"]:
+        st.session_state["values"] = True
+        st.text(
+            "Age: "
+            + str(age)
+            + "\nGender: "
+            + str(gender)
+            + "\ndust"
+            + str(dust)
+            + "\n hazard"
+            + str(hazard)
+            + "\n gene"
+            + str(gene)
+            + "\n lung_disesa"
+            + str(lung_disesa)
+            + "\n smokin"
+            + str(smokin)
+            + "\n pass_smok"
+            + str(pass_smok)
+            + "\n nails"
+            + str(nails)
+            + "\n cold"
+            + str(cold)
+        )
 
-        if st.button("Run Model"):
-            with st.spinner("Running Arima..."):
+        if st.button("Click on me to check your risk."):
+            with st.spinner("Fetching data from your local station and streamlit."):
+                result = model()
+            with st.spinner("Fetching forcasts from ARIMA..."):
                 result = model()
             st.success("AQI forecast ready! Running LSTM...")
             with st.spinner("Running LSTM..."):
                 result = model()
-            st.write("Result from model: ",result)
+            st.write("Model has finished running.")
+            result = "20.4%"
+            st.success("Your risk of lung cancer is: {}", result)
 
-        if st.button("Get data from thingspeak"):
-            with st.spinner("Getting data..."):
-                co_data = requests.get(co_url).json()
-                co_data = co_data['feeds']
-                co_df = pd.DataFrame(co_data)
-                
-                pm_data = requests.get(pm_url).json()
-                pm_data = pm_data['feeds']
-                pm_df = pd.DataFrame(pm_data)
+        # if st.button("Get data from thingspeak"):
+        #     with st.spinner("Getting data..."):
+        #         co_data = requests.get(co_url).json()
+        #         co_data = co_data['feeds']
+        #         co_df = pd.DataFrame(co_data)
+        #         pm_data = requests.get(pm_url).json()
+        #         pm_data = pm_data['feeds']
+        #         pm_df = pd.DataFrame(pm_data)
 
-        if co_df.empty == False and pm_df.empty == False:
-            co_col, pm_col = st.columns(2)
+        # if co_df.empty == False and pm_df.empty == False:
+        #     co_col, pm_col = st.columns(2)
+        #     with co_col:
+        #         st.title("CO Data")
+        #         st.dataframe(co_df)
+        #     with pm_col:
+        #         st.title("PM Data")
+        #         st.dataframe(pm_df)
 
-            with co_col:
-                st.title("CO Data")
-                st.dataframe(co_df)
-                
-            with pm_col:
-                st.title("PM Data")
-                st.dataframe(pm_df)
 
 if __name__ == "__main__":
     main()
