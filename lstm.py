9ca9bbe909276f6c8007d31dc214e5cf406958c4
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-11-07 18:27:44 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-import math
+import random
 
 
 def lstm(a):
     # DUMMY FUNCTION REMOVE LATER!!!!!!!!
-    a = [(i/7)*math.random(0.8,1) for i in a]
+    a = [(i/7)*random.uniform(0.8,1.0) for i in a]
+    s = sum(a)
+    return s
