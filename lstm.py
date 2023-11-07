1b549829c3dc36ee4faaa1755181f9fb783c846f
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-11-07 18:34:41 +0530

    scafolding done:)

--- lstm.py
+++ lstm.py
@@ -6,5 +6,7 @@ import random
 def lstm(a):
     # DUMMY FUNCTION REMOVE LATER!!!!!!!!
     a = [(i/8)*random.uniform(0.8,1.0) for i in a]
-    s = sum(a)*10
+    s = sum(a)*10 + 20
+    if s > 95:
+        s = 95.231
     return s
