3a26f69f2b7bf959f87e8cf53171c5fdc40a85a6
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2024-02-06 14:50:04 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -5,6 +5,10 @@ import random
 
 def lstm(a):
     a= a[1:]
+    if sum(a)<3:
+        a = [(i/8)*random.uniform(0.8,1.0) for i in a]
+        s = sum(a)*10
+        return s
     # DUMMY FUNCTION REMOVE LATER!!!!!!!!
     a = [(i/8)*random.uniform(0.8,1.0) for i in a]
     s = sum(a)*10 + 20
